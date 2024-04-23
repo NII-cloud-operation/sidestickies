d001eb15cca49d56cccb0284bc71523fbea8f3a3
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-04-23 16:22:48 +0900

    Fix tests

--- ui-tests/tests/nbtags.spec.ts
+++ ui-tests/tests/nbtags.spec.ts
@@ -1,4 +1,5 @@
-import { expect, test } from '@jupyterlab/galata';
+import { IJupyterLabPage, expect, test } from '@jupyterlab/galata';
+import { Page } from '@playwright/test';
 
 /**
  * Don't load JupyterLab webpage before running the tests.
@@ -6,7 +7,21 @@ import { expect, test } from '@jupyterlab/galata';
  */
 test.use({ autoGoto: false });
 
-test('should emit an activation console message', async ({ page }) => {
+// https://github.com/jupyterlab/jupyterlab/blob/9844a6fdb680aeae28a4d6238433f751ce5a6204/galata/src/fixtures.ts#L319-L336
+async function myWaitForApplication({ baseURL }, use) {
+  const waitIsReady = async (
+    page: Page,
+    helpers: IJupyterLabPage
+  ): Promise<void> => {
+    await page.waitForSelector('#jupyterlab-splash', {
+      state: 'detached'
+    });
+    //! not wait for launcher tab.
+  };
+  await use(waitIsReady);
+}
+
+test('should emit an activation console message', async ({ page, waitForApplication: myWaitForApplication }) => {
   const logs: string[] = [];
 
   page.on('console', message => {
