30dc2f018b60507b676b5f4662097a8240b82cf1
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-10-06 09:51:49 +0000

    Add WAIT_FOR_EP_WEAVE_READY environment variable to prevent waiting services on MyBinder

--- example/00-add-config.sh
+++ example/00-add-config.sh
@@ -7,8 +7,7 @@ TARGET_CONFIG=/etc/jupyter/jupyter_notebook_config.py
 echo >> ${TARGET_CONFIG}
 if [[ -f /opt/nbtags/config.py ]] ; then
   cat /opt/nbtags/config.py >> ${TARGET_CONFIG}
-fi
-if [[ ! -f /opt/nbtags/config.py ]] ; then
+else
   if [[ ! -f /opt/etherpad/APIKEY.txt ]] ; then
     cat /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 24 > /opt/etherpad/APIKEY.txt
   fi
